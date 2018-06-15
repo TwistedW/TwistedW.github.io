af62e1388486d7ca95590952b55eb5c8fe01f751
TwistedW <2359375192@qq.com>
2018-06-15 10:14:45 +0800

    updating

--- assets/js/myreward.js
+++ assets/js/myreward.js
@@ -2,7 +2,7 @@ $(function(){
 	$(".pay_item").click(function(){
 		$(this).addClass('checked').siblings('.pay_item').removeClass('checked');
 		var dataid=$(this).attr('data-id');
-		$(".shang_payimg img").attr("src","../assets/img/social/"+dataid+"speed.png");
+		$(".shang_payimg img").attr("/assets/img/social/"+dataid+"speed.png");
 		$("#shang_pay_txt").text(dataid=="Wechat"?"微信":"支付宝");
 	});
 });
